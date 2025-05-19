b2176d23bc10bd658d23b564a2eb94aff6c98a32
Nate Brevin Que <nate.que@student.ateneo.edu>
2025-05-19 22:49:56 +0800

    Added my solution to Round 1025 (Div.2) C2

---
+++ Contests/Round 1025 (Div. 2)/C2-HackingNumbersMedium.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for test in range(t):
+	n = int(input())
+
+	print("mul 9")
+	_ = input()
+
+	print("digit")
+	_ = input()
+
+	print("digit")
+	_ = input()
+	
+	print(f"add {n-9}")
+	_ = input()
+
+	print("!")
+	_ = input()
